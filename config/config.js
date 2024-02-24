a2bc23eb176571e94592f8aa916d8de28eee6628
ReeshU Raj <reeshu54raj@gmail.com>
2024-02-25 00:44:45 +0530

    Files added

---
+++ config/config.js
@@ -0,0 +1,7 @@
+var BASE_URL_UI = 'http://localhost:5500';
+var BASE_URL_SERVER = 'http://localhost:8080';
+
+// API Credentials
+// var API_USERNAME = 'root';
+// var API_PASSWORD = '1234';
+// var API_ENCODED_DATA = btoa(API_USERNAME + ':' + API_PASSWORD);
