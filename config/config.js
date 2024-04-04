640c691d50c8d7e3af0f4f5f179bf337bc33dad5
ReeshU Raj <reeshu54raj@gmail.com>
2024-04-04 14:43:41 +0530

    server urls

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
-var BASE_URL_UI = 'http://localhost:5500';
-var BASE_URL_SERVER = 'http://localhost:8080';
+// var BASE_URL_UI = 'http://localhost:5500';
+var BASE_URL_UI = 'http://revisor.projectgallery.online/';
+// var BASE_URL_SERVER = 'http://localhost:8080';
+var BASE_URL_SERVER = 'https://revisorspringboot-production.up.railway.app/';
 
 // API Credentials
 // var API_USERNAME = 'root';
